8c080958d097c83953b3ceeaf7313ce40de76249
Race428 <raceacheson@gmail.com>
2019-05-24 12:02:37 -0600

    additions

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+// import layout from "../components/layout";
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+  <div style={{ color: `teal` }}>
+
+    <p>Such wow. Very React.</p>
+
+  </div>
+  </Layout>
+)
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,12 @@
 import React from "react"
-import { Link} from 'gatsby'
 import Layout from '../components/layout'
 export default () => (
- <Layout> 
+  <Layout>
+   <div style ={{color: `pink`}}>
+  <div style={{ color: `teal` }}>
 
-
- <div>
-    <h1>About Gatsby</h1>
     <p>Such wow. Very React.</p>
-<Link to='/about'>
-    <div>Here is the link</div>
-    </Link>
   </div>
-
+  </div> 
   </Layout>
 )
